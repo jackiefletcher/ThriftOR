b5fc9051efb2bbf1afd538260b96d2fab76f7856
Gabe <gabefinch@gmail.com>
2015-04-07 15:32:32 -0700

    modified Spot model to display details for each spot via ember

--- ember/app/models/spot.js
+++ ember/app/models/spot.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  location: DS.attr('string'),
+  kind: DS.attr('string'),
+  price_range: DS.attr('string'),
+  style: DS.attr('string'),
+  hours: DS.attr('string'),
+  dates: DS.attr('string'),
+  phone: DS.attr('string'),
+  card: DS.attr('boolean')
+
 });
