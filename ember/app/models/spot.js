32a7d90383d0b868aaad65946272510e48c8f057
Lizzie Koehler <lizziefkoehler@gmail.com>
2015-04-08 14:33:55 -0700

    list thriftORs within 20 miles

--- ember/app/models/spot.js
+++ ember/app/models/spot.js
@@ -2,15 +2,16 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-    location: DS.attr('string'),
-    latitude: DS.attr('number'),
-    longitude: DS.attr('number'),
-    kind: DS.attr('string'),
-    price_range: DS.attr('string'),
-    style: DS.attr('string'),
-    hours: DS.attr('string'),
-    dates: DS.attr('string'),
-    phone: DS.attr('string'),
-    card: DS.attr('boolean'),
-    image_url: DS.attr('string')
+  location: DS.attr('string'),
+  latitude: DS.attr('number'),
+  longitude: DS.attr('number'),
+  kind: DS.attr('string'),
+  price_range: DS.attr('string'),
+  style: DS.attr('string'),
+  hours: DS.attr('string'),
+  dates: DS.attr('string'),
+  phone: DS.attr('string'),
+  card: DS.attr('boolean'),
+  image_url: DS.attr('string'),
+  nearbys: DS.attr()
 });
